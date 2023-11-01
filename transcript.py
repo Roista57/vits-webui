e66dbe1fdcf6b2346b6efe69fa0e258aeb57caa4
Roista57 <roista57@gmail.com>
2023-11-01 20:46:30 +0900

    fix && refactor: resample_wav, check_wav_files 함수를 추가하여 기존의 음성파일 -> whisper 형태에서 음성파일 -> 음성파일 체크 -> 음성파일 리샘플링 -> whisper 형태로 변경하였습니다.

--- transcript.py
+++ transcript.py
@@ -1,151 +1,147 @@
-import faster_whisper
 from faster_whisper import WhisperModel
-import os
-import torch
 import argparse
-from tqdm import tqdm
-import sys
 from pydub import AudioSegment
+import os
+import wave
+from tqdm import tqdm
 
 
-def resample_directory(wav_files, root, speaker_info, target_sample_rate=22050):
-    if speaker_info == 'Single':
-        replace_dir = ["audio_file/SP/", "filelists/SP/"]
+def resample_wav(input_wav_path, output_wav_path, target_sample_rate=22050):
+    if os.path.exists(output_wav_path):
+        with wave.open(output_wav_path, 'rb') as wf:
+            num_channels = wf.getnchannels()
+            sample_width = wf.getsampwidth()
+            sample_rate = wf.getframerate()
+            if num_channels == 1 and sample_width == 2 and sample_rate == target_sample_rate:
+                # print(f"{output_wav_path} already exists. Skipping...")
+                return
     else:
-        replace_dir = ["audio_file/MP/", "filelists/MP/"]
-    os.makedirs(root.replace(replace_dir[0], replace_dir[1]), exist_ok=True)
-    for wav in tqdm(wav_files, desc=f"resample ==> {root}"):
-        full_wav_path = os.path.join(root, wav)
-        output_path = full_wav_path.replace(replace_dir[0], replace_dir[1])
-        audio = AudioSegment.from_wav(full_wav_path)
+        if not check_wav_files(input_wav_path):
+            return
+    try:
+        audio = AudioSegment.from_wav(input_wav_path)
+        if len(audio) < 1000:
+            print(f"{input_wav_path} The length of the audio is less than 1 second.")
+            return
+        if len(audio) > 10000:
+            print(f"{input_wav_path} The length of the audio is long than 10 second.")
+            return
         resampled_audio = audio.set_frame_rate(target_sample_rate)
-        resampled_audio.export(output_path, format="wav")
+        # print(f"Trying to save to: {output_wav_path}")
+        resampled_audio.export(output_wav_path, format="wav")
+    except Exception as e:
+        print(f"Error processing {input_wav_path}: {e}")
 
 
-# faster_whisper를 사용하여 음성 파일을 읽은 뒤 대사를 반환합니다.
+def check_wav_files(wav_path):
+    try:
+        with wave.open(wav_path, 'rb') as wf:
+            """
+            num_channels = wf.getnchannels()
+            sample_width = wf.getsampwidth()
+            sample_rate = wf.getframerate()
+            
+            if num_channels != 1:
+                print(f"{wav_path}:{num_channels} is not mono.")
+            if sample_width != 2:
+                print(f"{wav_path}:{sample_width} does not have a sample width of 2.")
+            if sample_rate not in (8000, 16000, 32000, 48000):
+                print(f"{wav_path}:{sample_rate} does not have a valid sample rate.")
+            if num_channels == 1 and sample_width == 2 and sample_rate in (8000, 16000, 32000, 48000):
+                print(f"{wav_path} meets all the requirements.")
+            """
+            return True
+    except wave.Error as e:
+        print(f"Failed to open or read {wav_path} due to a WAV error: {e}")
+        return False
+
+
 def whisper_script(model, path, lang):
     segments, info = model.transcribe(path, language=lang, beam_size=5)
-    text = ""
-    for segment in segments:
-        text += segment.text
-    if len(text) > 0 and text[0] == ' ':
-        return text[1:]
-    else:
-        return text
+    text = "".join(segment.text for segment in segments).strip()
+    return text
 
 
-def process_wav_files(model, files, base_path, output, lang, idx=None):
-    for wav in files:
-        full_wav_path = os.path.join(base_path, wav)
-        text = whisper_script(model, full_wav_path, lang)  # 음성 파일을 처리하는 함수.
-        if len(text) == 0:
-            print(f"Error: {full_wav_path} 파일의 대본을 추출하지 못했습니다.")
-        else:
-            print_line = f"{full_wav_path}|{text}" if idx is None else f"{full_wav_path}|{idx}|{text}"
-            print(print_line)
-            output.write(print_line + "\n")
+def get_speaker_index(filename, speakers_mapping):
+    # 상위 폴더의 이름을 가져온다.
+    speaker_folder_name = os.path.basename(os.path.dirname(filename))
+    # 해당 이름을 기반으로 index를 반환한다.
+    return speakers_mapping[speaker_folder_name]
 
-
-
-def process_wav_files_tqdm(model, files, base_path, output, lang, idx=None):
-    for wav in tqdm(files, desc=base_path):
-        full_wav_path = os.path.join(base_path, wav)
-        text = whisper_script(model, full_wav_path, lang)  # 음성 파일을 처리하는 함수.
-        if len(text) == 0:
-            print(f"Error: {full_wav_path} 파일의 대본을 추출하지 못했습니다.")
-        else:
-            print_line = f"{full_wav_path}|{text}" if idx is None else f"{full_wav_path}|{idx}|{text}"
-            output.write(print_line + "\n")
-
-
-# 추출된 대사를 보여주지만 예측하는 작업 종료시간을 보여주지 않습니다.
-def run_whisper(speaker, lang, target_sample):
-    model_size = "large-v2"
-    if torch.cuda.is_available():
-        print('GPU 모델 사용')
-        model = WhisperModel(model_size, device="cuda", compute_type="float16")
-    else:
-        print('CPU 모델 사용')
-        model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-    transcript_path = "filelists/filelists.txt"
-
-    if speaker == 'Single':
-        print("단일 화자 대사 작업 시작")
-        replace_dir = ["audio_file/SP/", "filelists/SP/"]
-        input_folder_path = replace_dir[0]
-        output_folder_path = replace_dir[1]
-        wav_files = [f for f in os.listdir(input_folder_path) if f.endswith('.wav')]
-        with open(transcript_path, "w", encoding="utf-8") as output:
-            resample_directory(wav_files, input_folder_path, speaker, target_sample)
-            process_wav_files(model, wav_files, output_folder_path, output, lang)
-
-    elif speaker == 'Multi':
-        print("다중 화자 대사 작업 시작")
-        replace_dir = ["audio_file/MP/", "filelists/MP/"]
-        base_path = replace_dir[0]
-        speakers = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
-        with open(transcript_path, "w", encoding="utf-8") as output:
-            for idx, speaker in enumerate(speakers):
-                speaker_path = os.path.join(base_path, speaker)
-                for root, dirs, files in os.walk(speaker_path):
-                    wav_files = [f for f in files if f.endswith('.wav')]
-                    output_path = root.replace(replace_dir[0], replace_dir[1])
-                    resample_directory(wav_files, root, speaker, target_sample)
-                    process_wav_files(model, wav_files, output_path, output, lang, idx)
-
-
-# tqdm을 사용하여 작업 종료시간을 예측해주지만 추출된 대사를 보여주지 않습니다.
-def run_whisper_tqdm(speaker, lang, target_sample):
-    model_size = "large-v2"
-    if torch.cuda.is_available():
-        print('GPU 모델 사용')
-        model = WhisperModel(model_size, device="cuda", compute_type="float16")
-    else:
-        print('CPU 모델 사용')
-        model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-    transcript_path = "filelists/filelists.txt"
-
-    if speaker == 'Single':
-        print("단일 화자 대사 작업 시작")
-        replace_dir = ["audio_file/SP/", "filelists/SP/"]
-        input_folder_path = replace_dir[0]
-        output_folder_path = replace_dir[1]
-        wav_files = [f for f in os.listdir(input_folder_path) if f.endswith('.wav')]
-        with open(transcript_path, "w", encoding="utf-8") as output:
-            resample_directory(wav_files, input_folder_path, speaker, target_sample)
-            process_wav_files_tqdm(model, wav_files, output_folder_path, output, lang)
-
-    elif speaker == 'Multi':
-        print("다중 화자 대사 작업 시작")
-        replace_dir = ["audio_file/MP/", "filelists/MP/"]
-        base_path = replace_dir[0]
-        speakers = [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))]
-        with open(transcript_path, "w", encoding="utf-8") as output:
-            for idx, speaker in enumerate(speakers):
-                speaker_path = os.path.join(base_path, speaker)
-                for root, dirs, files in os.walk(speaker_path):
-                    wav_files = [f for f in files if f.endswith('.wav')]
-                    output_path = root.replace(replace_dir[0], replace_dir[1])
-                    resample_directory(wav_files, root, speaker, target_sample)
-                    process_wav_files_tqdm(model, wav_files, output_path, output, lang, idx)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--speaker", type=str)
-    parser.add_argument("--language", type=str)
-    parser.add_argument("--samplerate", type=int)
-    parser.add_argument("--tqdm", type=str)
-
-    args = parser.parse_args()
+def run_whisper_to_script(args):
+    base_input_dir = 'audio'
+    base_output_dir = 'filelists'
 
     speaker = args.speaker
-    lang = args.language
+    language = args.language
     sample_rate = args.samplerate
     run_tqdm = args.tqdm
 
-    if run_tqdm == 'True':
-        run_whisper_tqdm(speaker, lang, sample_rate)
+    try:
+        model_size = "large-v2"
+        model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    except Exception as e:
+        print(f"Error initializing Whisper model: {e}. Exiting...")
+        return
+
+    if not os.path.exists(base_output_dir):
+        os.makedirs(base_output_dir)
+
+    input_dir = os.path.join(base_input_dir, "SP" if speaker == "Single" else "MP")
+    output_dir = os.path.join(base_output_dir, "SP" if speaker == "Single" else "MP")
+
+    filenames = [os.path.join(dirpath, filename) for dirpath, _, files in os.walk(input_dir) for filename in files if filename.endswith('.wav')]
+    iterator = tqdm(filenames, desc="Processing", disable=not run_tqdm)
+
+    if speaker == "Multi":
+        # audio/MP 폴더 내의 모든 폴더들의 이름을 가져와 순서대로 index를 부여
+        speaker_folders = sorted([folder for folder in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, folder))])
+        speakers_mapping = {name: idx for idx, name in enumerate(speaker_folders)}
     else:
-        run_whisper(speaker, lang, sample_rate)
+        speakers_mapping = {}
+
+    with open("filelists/filelists.txt", "w", encoding='utf-8') as file:
+        for filename in iterator:
+            try:
+                if not os.access(filename, os.R_OK):
+                    print(f"Error: No read access to {filename}. Skipping...")
+                    continue
+
+                output_wav_path = os.path.abspath(os.path.join(output_dir, os.path.relpath(filename, input_dir)))
+                output_dir_path = os.path.dirname(output_wav_path)
+
+                # print(f"Checking existence of directory: {output_dir_path}")
+                if not os.path.exists(output_dir_path):
+                    # print(f"Creating directory: {output_dir_path}")
+                    os.makedirs(output_dir_path)
+
+                resample_wav(filename, output_wav_path, sample_rate)
+                text = whisper_script(model, output_wav_path, language)
+
+                # 기존 코드에서 speaker 처리 부분을 요구사항에 맞게 수정
+                if speaker == "Multi":
+                    idx = get_speaker_index(filename, speakers_mapping)
+                    file.write(f"{output_wav_path}|{idx}|{text}\n")
+                    if not run_tqdm:
+                        print(f"{output_wav_path}|{idx}|{text}\n")
+                else:
+                    file.write(f"{output_wav_path}|{text}\n")
+                    if not run_tqdm:
+                        print(f"{output_wav_path}|{text}\n")
+            except Exception as e:
+                print(f"Error processing {filename} with Whisper or during resampling: {e}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--speaker", type=str, choices=['Single', 'Multi'], required=True, help="Specify if it's single or multi speaker.")
+    parser.add_argument("--language", type=str, required=True, help="Language of the audio files.")
+    parser.add_argument("--samplerate", type=int, default=22050, help="Target sample rate for resampling.")
+    parser.add_argument("--tqdm", action="store_true", help="Show progress bar.")
+
+    args = parser.parse_args()
+
+    try:
+        run_whisper_to_script(args)
+    except Exception as e:
+        print(f"Error running script: {e}")
